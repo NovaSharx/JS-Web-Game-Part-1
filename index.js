1b7519626413d30c893129d76e7d3a2f6302cc7f
Davin <davin-dj@hotmail.com>
2022-08-24 16:06:16 -0400

    Added a new item function to spawn sword.png.

--- index.js
+++ index.js
@@ -7,9 +7,24 @@ function newImage(imgAddress, left, bottom) {
     document.body.append(image)
 }
 
+function newItem(imgAddress, left, bottom) {
+    let image = document.createElement('img')
+    image.src = imgAddress
+    image.style.position = 'fixed'
+    image.style.left = left
+    image.style.bottom = bottom
+    document.body.append(image)
+}
+
 newImage('assets/green-character.gif','100px','100px')
 newImage('assets/pine-tree.png','450px','200px')
 newImage('assets/tree.png','200px','300px')
 newImage('assets/pillar.png','350px','100px')
 newImage('assets/crate.png','150px','200px')
 newImage('assets/well.png','500px','425px')
+
+newItem('assets/sword.png', '500px', '405px')
+
+sword.addEventListener('click', function() {
+    sword.remove()
+})
