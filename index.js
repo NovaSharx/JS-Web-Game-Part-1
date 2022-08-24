ed52fcb91b5a990d61dde1787ac2eabc58398093
Davin <davin-dj@hotmail.com>
2022-08-24 15:58:09 -0400

    Created a function to spawn new assets.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(imgAddress, left, bottom) {
+    let image = document.createElement('img')
+    image.src = imgAddress
+    image.style.position = 'fixed'
+    image.style.left = left
+    image.style.bottom = bottom
+    document.body.append(image)
+}
 
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)
-
+newImage('assets/green-character.gif','100px','100px')
+newImage('assets/pine-tree.png','450px','200px')
+newImage('assets/tree.png','200px','300px')
+newImage('assets/pillar.png','350px','100px')
+newImage('assets/crate.png','150px','200px')
+newImage('assets/well.png','500px','425px')
