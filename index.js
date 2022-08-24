be342c4225c4a30ecc8176d98e997e6b674d2161
Davin <davin-dj@hotmail.com>
2022-08-24 17:42:13 -0400

    Added a background rendering function.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function newImage(imgAddress, left, bottom) {
     let image = document.createElement('img')
     image.src = imgAddress
     image.style.position = 'fixed'
-    image.style.left = left
-    image.style.bottom = bottom
+    image.style.left = left+'px'
+    image.style.bottom = bottom+'px'
     document.body.append(image)
     return image
 }
@@ -16,13 +16,29 @@ function newItem(imgAddress, left, bottom) {
     })
 }
 
-newImage('assets/green-character.gif','100px','100px')
-newImage('assets/pine-tree.png','450px','200px')
-newImage('assets/tree.png','200px','300px')
-newImage('assets/pillar.png','350px','100px')
-newImage('assets/crate.png','150px','200px')
-newImage('assets/well.png','500px','425px')
+function newBackground(imgAddress, left, bottom) {
+    let numTilesRow = Math.ceil(window.innerWidth/100)
+    let numTilesColumn = Math.ceil(bottom/100)
 
-newItem('assets/sword.png', '500px', '405px')
-newItem('assets/shield.png', '165px', '185px')
-newItem('assets/staff.png', '600px', '100px')
+    for (let y = numTilesColumn; y > 0; y--) {
+        bottom-=100
+        for (let x = 0; x < numTilesRow; x++) {
+            let background = newImage(imgAddress, left+x*100, bottom)
+            background.style.zIndex = -1
+        }
+    }
+}
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+
+newBackground('assets/sky.png', 0, window.innerHeight)
+newBackground('assets/grass.png', 0, 500)
